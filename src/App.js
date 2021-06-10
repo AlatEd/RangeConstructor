e81fa26f5fb9a20a65f7c89b350d4e3dc7340b96
babafin <petehki@outlook.com>
2021-06-10 21:10:02 +0300

    toka testi

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          HELLO WORLD!!!
+          HELLO WORLD!!! Toka
         </a>
       </header>
     </div>
