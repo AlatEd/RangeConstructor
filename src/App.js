648e094284f8eebdd9d25065d0b0f5d05406419f
babafin <petehki@outlook.com>
2021-06-10 23:55:06 +0300

    added test component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import "./App.css";
-import logo from "./logo.svg";
+import ClickMe from "./ClickMe";
 // test
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          HELLO WORLD!!! Poistettu tekstiä
-        </a>
-      </header>
+    <div>
+      <ClickMe />
     </div>
   );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         HELLO WORLD!!! Poistettu
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
