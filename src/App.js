c403df8611c4f359493019476f9eca5d8251eba7
Edgar Alatalo <edgar.alatalo@gmail.com>
2021-06-10 21:25:47 +0300

    Line ending test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          HELLO WORLD!!! Toka Kolmas
+          HELLO WORLD!!! Line ending test
         </a>
       </header>
     </div>
