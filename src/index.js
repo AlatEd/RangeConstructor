a807a17ac967d1c3a950aa6da1bbeea185ba9404
Edgar Alatalo <edgar.alatalo@gmail.com>
2021-06-17 21:50:44 +0300

    Apollo and some other dependencies added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'https://graphql-pokemon.now.sh/'
+})
+
+const client = new ApolloClient({
+  cache,
+  link
+})
 
 ReactDOM.render(
   <React.StrictMode>
